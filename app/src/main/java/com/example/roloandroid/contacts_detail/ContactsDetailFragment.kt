8e87d92e9f249cbb8f5e7de3f54ac0d9e2d7f5cc
alby6 <albert@xuelei.com.tw>
2020-08-19 03:02:11 +0800

    Resolved modal UI for ContactsDetailFragment.

--- app/src/main/java/com/example/roloandroid/contacts_detail/ContactsDetailFragment.kt
+++ app/src/main/java/com/example/roloandroid/contacts_detail/ContactsDetailFragment.kt
@@ -2,6 +2,7 @@ package com.example.roloandroid.contacts_detail
 
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.util.DisplayMetrics
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +11,8 @@ import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.example.roloandroid.R
 import com.example.roloandroid.databinding.ContactsDetailFragmentBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -23,6 +26,12 @@ class ContactsDetailFragment : BottomSheetDialogFragment() {
 
     val viewModel: ContactsDetailViewModel by viewModels()
 
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NORMAL, R.style. AppBottomSheetDialogTheme);
+
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -36,6 +45,15 @@ class ContactsDetailFragment : BottomSheetDialogFragment() {
             vm = viewModel
             setLifecycleOwner {  this@ContactsDetailFragment.lifecycle}
         }
+
+        (dialog as? BottomSheetDialog)?.let {
+            it.behavior.peekHeight = (requireContext()
+                .resources
+                .displayMetrics
+                .heightPixels * 0.9)
+                .toInt()
+        }
+
         return bind.root
 
     }
